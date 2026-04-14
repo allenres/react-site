58b0d19d4eeca158835001340cfa5d67b78b690f
allenres <allen.resulidze@gmail.com>
2026-04-14 11:23:22 -0700

    add local image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
                 <p>Seattle is a vibrant city in the Pacific Northwest known for its stunning natural surroundings, with water, mountains, and evergreen forests all nearby. It’s home to iconic landmarks like the Space Needle and the bustling Pike Place Market, where visitors can explore local food, art, and culture. Seattle also has a rich music and tech scene, being the birthplace of artists like Nirvana and the headquarters of major companies like Amazon and Microsoft. Known for its rainy weather and coffee culture, Seattle offers a unique blend of urban energy and natural beauty.</p>
             </div>
             <div className='image-container'>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/5/58/Seattle_Center_as_night_falls.jpg" alt="A view of seattle" />
+                <img src="./Seattle_Center_as_night_falls.jpg" alt="A view of seattle" />
             </div>
         </div>
     );
