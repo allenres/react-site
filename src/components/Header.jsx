127d8ff891651621b456eac0e8af9cfdf0e55c7c
allenres <allen.resulidze@gmail.com>
2026-04-14 10:53:24 -0700

    Header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import './Header.css';
 
 export default function Header({title}) {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
             <nav>
-                <ul>
+                <ul className='nav-links'>
                     <li><a href="#">History</a></li>
                     <li><a href="#">Things To See</a></li>
                 </ul>
