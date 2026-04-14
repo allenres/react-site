c88d7a24cc1490377eab2308d149a308a8c310cd
allenres <allen.resulidze@gmail.com>
2026-04-14 11:06:37 -0700

    Hero content and styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,15 @@
-function Hero() {
+import './Hero.css';
+
+export default function Hero() {
     return (
-        <div class="Hero">
-            <h2>Hero text</h2>
-            <p>fdsfsdafdasfsdfsafdsafsfsda.fd adfsd</p>
+        <div class="hero-section">
+            <div>
+                <h2>About Seattle</h2>
+                <p>Seattle is a vibrant city in the Pacific Northwest known for its stunning natural surroundings, with water, mountains, and evergreen forests all nearby. It’s home to iconic landmarks like the Space Needle and the bustling Pike Place Market, where visitors can explore local food, art, and culture. Seattle also has a rich music and tech scene, being the birthplace of artists like Nirvana and the headquarters of major companies like Amazon and Microsoft. Known for its rainy weather and coffee culture, Seattle offers a unique blend of urban energy and natural beauty.</p>
+            </div>
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/58/Seattle_Center_as_night_falls.jpg" alt="A view of seattle" />
+            </div>
         </div>
     );
 }
-
-export default Hero;
